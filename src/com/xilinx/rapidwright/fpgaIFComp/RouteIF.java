63995fa76a3989bf29eb04ea4ace3e833d2f6aba
Zak Nafziger <zak.nafziger@amd.com>
2023-05-24 15:35:00 -0700

    modify route IF to use multiple routers
    Signed-off-by: Zak Nafziger <zak.nafziger@amd.com>

--- src/com/xilinx/rapidwright/fpgaIFComp/RouteIF.java
+++ src/com/xilinx/rapidwright/fpgaIFComp/RouteIF.java
@@ -30,6 +30,7 @@ import com.xilinx.rapidwright.interchange.LogNetlistReader;
 import com.xilinx.rapidwright.interchange.LogNetlistWriter;
 import com.xilinx.rapidwright.interchange.PhysNetlistReader;
 import com.xilinx.rapidwright.interchange.PhysNetlistWriter;
+import com.xilinx.rapidwright.router.Router;
 import com.xilinx.rapidwright.rwroute.PartialRouter;
 
 
@@ -40,6 +41,20 @@ import com.xilinx.rapidwright.rwroute.PartialRouter;
  *
  */
 public class RouteIF {
+
+    private EDIFNetlist n;
+    private Design d;
+
+    private void readUnroutedDesign(String logNlistFileName, String physNlistFileName) throws IOException {
+        n = LogNetlistReader.readLogNetlist(logNlistFileName);
+        d = PhysNetlistReader.readPhysNetlist(physNlistFileName, n);
+    }
+
+    private void writeRoutedDesign(String logNlistFileName, String physNlistFileName) throws IOException {
+        LogNetlistWriter.writeLogNetlist(d.getNetlist(), logNlistFileName.replace(".netlist", "_routed.netlist"));
+        PhysNetlistWriter.writePhysNetlist(d, physNlistFileName.replace(".phys", "_routed.phys"));
+    }
+
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.out.println(
@@ -49,12 +64,46 @@ public class RouteIF {
         String logNlistFileName = args[0];
         String physNlistFileName = args[1];
 
-        EDIFNetlist n = LogNetlistReader.readLogNetlist(logNlistFileName);
-        Design d = PhysNetlistReader.readPhysNetlist(physNlistFileName, n);
+        RouteIF R = new RouteIF();
+        rwRouter r = R.new rwRouter();
+        r.route(logNlistFileName, physNlistFileName);
+    }
 
-        d = PartialRouter.routeDesignPartialNonTimingDriven(d, null, true);
-        
-        LogNetlistWriter.writeLogNetlist(d.getNetlist(), logNlistFileName.replace(".netlist", "_routed.netlist"));
-        PhysNetlistWriter.writePhysNetlist(d, physNlistFileName.replace(".phys", "_routed.phys"));
+    public void route(String logNlistFileName, String physNlsitFileName) throws IOException{
+        throw new java.io.IOException();
+    }
+
+    public class rwRouter extends RouteIF {
+        @Override
+        public void route(String logNlistFileName, String physNlistFileName) throws IOException {
+            readUnroutedDesign(logNlistFileName, physNlistFileName);
+
+            PartialRouter.routeDesignPartialNonTimingDriven(d, null, true);
+
+            writeRoutedDesign(logNlistFileName, physNlistFileName);
+        }
+    }
+
+    public class classicRouter extends RouteIF{
+        @Override
+        public void route(String logNlistFileName, String physNlistFileName) throws IOException {
+            readUnroutedDesign(logNlistFileName, physNlistFileName);
+
+            Router r = new Router(d);
+            r.routeDesign();
+
+            writeRoutedDesign(logNlistFileName, physNlistFileName);
+        }
+    }
+
+    public class nullRouter extends RouteIF{
+        @Override
+        public void route(String logNlistFileName, String physNlistFileName) throws IOException {
+            readUnroutedDesign(logNlistFileName, physNlistFileName);
+
+            // do nothing
+
+            writeRoutedDesign(logNlistFileName, physNlistFileName);
+        }
     }
 }
